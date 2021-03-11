1d7a389e0bc35dc30d629a92287d8cce3b37f0b2
A-very-Cunning-ham <ave210knife@gmail.com>
2021-03-11 13:02:00 -0500

    Finish lab 5

--- lab5_template.py
+++ lab5_template.py
@@ -1,7 +1,7 @@
 '''
-Created on _______________________
-@author:   _______________________
-Pledge:    _______________________
+Created on 3/11/21
+@author: Avery Cunningham
+Pledge: I pledge my honor that I have abided by the Stevens Honor System
 
 CS115 - Lab 5
 '''
@@ -11,17 +11,58 @@ from cs115 import map
 words = []
 HITS = 10
 
+
+def ED(first, second):
+    ''' Returns the edit distance between the strings first and second.'''
+    if first == '':
+        return len(second)
+    elif second == '':
+        return len(first)
+    elif first[0] == second[0]:
+        return ED(first[1:], second[1:])
+    else:
+        substitution = 1 + ED(first[1:], second[1:])
+        deletion = 1 + ED(first[1:], second)
+        insertion = 1 + ED(first, second[1:])
+    return min(substitution, deletion, insertion)
+
+memo = {}
+
 def fastED(first, second):
     '''Returns the edit distance between the strings first and second. Uses
     memoization to speed up the process.'''
-    pass  # TODO
+    if (first, second) in memo:
+        return memo[(first, second)]
+    elif first == '':
+        memo[(first, second)] = len(second)
+        return len(second)
+    elif second == '':
+        memo[(first, second)] = len(first)
+        return len(first)
+    elif first[0] == second[0]:
+        ans = ED(first[1:], second[1:])
+        memo[(first, second)] = ans
+        return ans
+    else:
+        substitution = 1 + ED(first[1:], second[1:])
+        deletion = 1 + ED(first[1:], second)
+        insertion = 1 + ED(first, second[1:])
+        ans = min(substitution, deletion, insertion)
+        memo[(first, second)] = ans
+        return ans
+
+print(ED("spam", "xsam"))
+print(fastED("antidisestablishment", "antiquities"))
+print(fastED("antidisestablishment", "antiquities"))
+print(fastED("lower", "hover"))
 
 def getSuggestions(user_input):
     '''For each word in the global words list, determine the edit distance of
     the user_input and the word. Return a list of tuples containing the
     (edit distance, word).
     Hint: Use map and lambda, and it's only one line of code!'''
-    pass  # TODO
+    return map(lambda x: (fastED(x, user_input),x), words)
+    
 
 def spam():
     '''Main loop for the program that prompts the user for words to check.
@@ -45,6 +86,7 @@ def spam():
                 print(' %s' % suggestion[1])
             print('Computation time:', endTime - start_time, 'seconds')
     print('Bye')
+
 
 if __name__ == '__main__':
     f = open('3esl.txt')
