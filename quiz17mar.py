76380f0e31ae9bab26a944d35ca4c4c845b783b7
A-very-Cunning-ham <ave210knife@gmail.com>
2021-03-17 16:43:32 -0400

    Update quiz17mar.py

--- quiz17mar.py
+++ quiz17mar.py
@@ -2,6 +2,7 @@
 
 # You can use 'in', len, map, filter, and reduce, but try to solve it without
 # using fancy Python library functions.
+# Avery Cunningham
 
 from cs115 import *
 
@@ -18,14 +19,14 @@ listOfWords = S.split()
 def deVowel(w):
     """Assuming w is a word, remove its vowels.
     For example, deVowel('friday') is 'frdy'."""
-    return None # TO-DO
+    return reduce(lambda x, y: x + y, filter(lambda x: x not in Vowels, w))
 
 '''Now, change the expression in this assignment so it sets longest to
 be one of the longest words in listOfWords, ignoring vowels.  One correct
 answer is 'students'. '''
 
 
-longest = None # TO-DO
+longest = reduce(lambda x,y: y if y[1] > x[1] else x,map(lambda x: [x, len(deVowel(x))], listOfWords))
                                                              
 print(longest)
 
