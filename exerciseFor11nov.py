17f00bbeb1a8ada422303691e2e76d487050d9d3
A-very-Cunning-ham <ave210knife@gmail.com>
2021-04-12 10:02:18 -0400

    Create exerciseFor11nov.py

---
+++ exerciseFor11nov.py
@@ -0,0 +1,12 @@
+# class exercise using for-loop
+
+from cs115 import *
+
+def mapSqr(L):
+    '''Assume L is a list of numbers; return map(sqr,L).
+    Use a for-loop.'''
+    pass
+
+def testMapSqr():
+    assert mapSqr([1,2,3]) == map(lambda x: x*x, [1,2,3])
+    
