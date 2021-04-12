2eeeb433a389d136177cf197a4c6d17b1d07a185
A-very-Cunning-ham <ave210knife@gmail.com>
2021-04-12 10:22:45 -0400

    Complete in class exercise

--- exerciseFor11nov.py
+++ exerciseFor11nov.py
@@ -2,11 +2,26 @@
 
 from cs115 import *
 
+
 def mapSqr(L):
     '''Assume L is a list of numbers; return map(sqr,L).
     Use a for-loop.'''
-    pass
+    result = []
+    for i in L:
+        # result += [i*i]
+        result.append(i * i)
+    return result
+
+
+def mapSqr2(L):
+    '''Assume L is a list of numbers; return map(sqr,L).
+    Use a for-loop.'''
+    return [i * i for i in L]
+
 
 def testMapSqr():
-    assert mapSqr([1,2,3]) == map(lambda x: x*x, [1,2,3])
-    
+    assert mapSqr([1, 2, 3]) == map(lambda x: x * x, [1, 2, 3])
+    assert mapSqr2([1, 2, 3]) == map(lambda x: x * x, [1, 2, 3])
+
+
+testMapSqr()
