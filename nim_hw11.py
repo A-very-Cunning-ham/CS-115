3d55c7778448f55fc4215860f43af97f5d5fdfe3
A-very-Cunning-ham <ave210knife@gmail.com>
2021-04-28 20:10:38 -0400

    Complete hw11

--- nim_hw11.py
+++ nim_hw11.py
@@ -15,14 +15,14 @@ def play_nim():
         display_piles()
         if sum(piles) == 0:
 
-            # TODO print a message telling the user they won
+            print("You win, good job!")
 
             break
         computer_plays()
         display_piles()
         if sum(piles) == 0:
 
-            # TODO print a message telling the user the computer won
+            print("Better luck next time, I win this one")
 
             break
 
@@ -35,15 +35,25 @@ def init_piles():
     global piles
     global num_piles
     
-    pass # TODO
+    num_piles = -1
+    while num_piles < 1:
+        num_piles = int(input("Enter how many piles do you want to play with: "))
 
+    piles = [0]*num_piles
+
+    for i in range(num_piles):
+        choice = -1
+        while choice < 1:
+            choice = int(input(f"Enter how many pieces you want in pile #{i}: "))
+        piles[i] = choice
         
 def display_piles():
     """ display current amount in each pile """
     global piles
     global num_piles
 
-    pass # TODO 
+    for idx, val in enumerate(piles):
+        print(f"pile {idx} = {val}")
 
 
 def user_plays():
@@ -63,7 +73,12 @@ def get_pile():
     global piles
     global num_piles
 
-    pass # TODO
+    choice = -1
+
+    while choice not in range(len(piles)):
+        choice = int(input(f"Enter which pile you choose: "))
+
+    return choice
 
 
 def get_number(pnum):
@@ -71,8 +86,14 @@ def get_number(pnum):
         Keep prompting until the amount is valid, i.e., at least 1
         and at most the amount in the pile."""
     global piles
-    
-    pass # TODO
+
+    choice = -1
+
+    while choice not in range(1, piles[pnum]+1):
+        choice = int(input(f"Enter how many to remove: "))
+        
+    return choice
+
 
 
 def game_nim_sum():
@@ -80,7 +101,12 @@ def game_nim_sum():
     global piles
     global num_piles 
 
-    pass # TODO 
+    nim_sum = 0
+
+    for val in piles:
+        nim_sum = nim_sum^val
+    
+    return nim_sum
 
 
 def opt_play():
@@ -93,7 +119,16 @@ def opt_play():
     global piles
     global num_piles 
 
-    pass # TODO 
+    nim_sum = game_nim_sum()
+    pile_sums = []
+    for idx, val in enumerate(piles):
+        pile_sum = val^nim_sum
+        if pile_sum < val:
+            return(idx, val-pile_sum)
+
+    for idx, val in enumerate(piles):
+        if val > 0:
+            return(idx, 1)
 
 
 def computer_plays():
@@ -103,9 +138,10 @@ def computer_plays():
     global piles
     global num_piles
 
-    pass # TODO
-
-    
+    print("It's my turn now, watch out")
+    opt = opt_play()
+    print(f"I remove {opt[1]} from pile {opt[0]}")
+    piles[opt[0]] -= opt[1]
 
 
 #   start playing automatically
